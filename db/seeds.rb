03f035a553f787849a7d555a3eedafe4f60ce9d1
atk <atksuimiy656@gmail.com>
2016-09-17 07:20:27 +0000

    change question seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-# if Question.count == 0
-#   Question.create([
-#     {content: "何時間寝ましたか?", select1: '0-4', select2: '5-', select1: '0-4', select1: '0-4', select1: '0-4'},
-#     {content: "何時間寝ましたか?", select1: '0-4', select2: '5-', select1: '0-4', select1: '0-4', select1: '0-4'},
-#     {content: "何時間寝ましたか?", select1: '0-4', select2: '5-', select1: '0-4', select1: '0-4', select1: '0-4'}
-#   ])
-# end
+if Question.count == 0
+  Question.create([
+    {content: "今日は元気ですか?", select1: '全く元気でない', select2: 'あまり元気でない', select3: '通常運転だ', select4: 'かなり元気だ', select5: '超元気だ'},
+    {content: "快眠でしたか?", select1: 'ほとんど寝ていない', select2: 'あまり寝ていない', select3: '通常運転だ', select4: '結構寝た', select5: '超快眠だった'},
+    {content: "食欲はわきますか?", select1: '何も食べたくない', select2: 'あまり食べたくない', select3: '通常運転だ', select4: 'かなり食べたい', select5: '超食べたい'},
+    {content: "自分の未来は明るいと思いますか?", select1: '全く思えない', select2: 'あまり思えない', select3: '通常運転だ', select4: 'かなり思える', select5: '超思える'},
+    {content: "助けてくれる人の存在を感じますか?", select1: '全く感じない', select2: 'あまり感じない', select3: '通常運転だ', select4: 'かなり感じる', select5: '超感じる'},
+    {content: "腸の調子はいいですか?", select1: '全く良くない', select2: 'あまり良くない', select3: '通常運転だ', select4: 'かなり快調だ', select5: '超快調だ'},
+    {content: "周りから嫌われても，自分を統制できますか?", select1: '全然できない', select2: 'あまりできない', select3: '通常運転だ', select4: 'かなりできる', select5: '超できる'},
+    {content: "幸せになれると思いますか?", select1: '全く思えない', select2: 'あまり思えない', select3: '通常運転だ', select4: 'かなり思える', select5: '超思える'},
+    {content: "自分の価値観は大事だと思いますか?", select1: '全く思えない', select2: 'あまり思えない', select3: '通常運転だ', select4: 'かなり思える', select5: '超思える'},
+    {content: "仕事はできそうですか?", select1: '全然手につかない', select2: 'あまりしたくない', select3: '通常運転だ', select4: 'かなり仕事したい', select5: '超仕事したい'}
+  ])
+end
 
 if Feel.count == 0
   Feel.create([
